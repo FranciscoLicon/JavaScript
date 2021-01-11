0e3393939ca6e07304287b1abdcc0792ba030ca3
Francisco Arturo Licon Colon <franciscolicon@localhost.localdomain>
2021-01-10 19:50:51 -0600

    Cola

---
+++ Estructuras de Datos/cola.js
@@ -0,0 +1,27 @@
+class Cola {
+    constructor() {
+        this.cola = [];
+    }
+
+    enqueue(elemento) {
+        this.cola.push(elemento);
+        return this.cola;
+    }
+
+    dequeue() {
+        return this.cola.shift();
+    }
+
+    peek() {
+        return this.cola[0];
+    }
+
+    size() {
+        return this.cola.length;
+    }
+
+    isEmpty() {
+        return this.cola.length === 0;
+    }
+
+}
