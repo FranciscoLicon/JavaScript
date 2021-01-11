8eaef4b2714fca08a0e56dc210a9751caac1f638
Francisco Arturo Licon Colon <franciscolicon@localhost.localdomain>
2021-01-10 19:42:36 -0600

    Pila

---
+++ Estructuras de Datos/pila.js
@@ -0,0 +1,26 @@
+class Pila {
+    constructor(){
+        this.pila = [];
+    }
+
+    push(elemento){
+        this.pila.push(elemento);
+        return this.pila;
+    }
+
+    pop(){
+        return this.pila.pop();
+    }
+
+    peek(){
+        return this.pila[this.pila.length-1];
+    }
+
+    size(){
+        return this.pila.length;
+    }
+
+    isEmpty(){
+        return this.pila.length === 0;
+    }
+}
